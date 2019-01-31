3b97f52e35b6500df463385a35094497008fb3c3
Ryan Spencer <ryan.spencer@gtri.gatech.edu>
2019-01-31 13:06:48 -0500

    fix comment lines in bash scripts, doh!

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,6 @@ hostname
 pwd
 ls -alh
 
-// create database table and load data
+# create database table and load data
 mysql --user=test --password=test -h 127.0.0.1 testDB <scripts/create-tables.sql
 mysql --user=test --password=test -h 127.0.0.1 testDB <scripts/load-data.sql
