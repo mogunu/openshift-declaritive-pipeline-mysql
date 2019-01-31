413423b083d0ed0ecd54f7a9c070cf5b76e9225a
Ryan Spencer <ryan.spencer@gtri.gatech.edu>
2019-01-31 14:07:14 -0500

    print mysql version numbers

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ pwd
 ls -alh
 
 # create database table and load data
+mysql -V
 mysql --user=test --password=test -h 127.0.0.1 -e "SHOW VARIABLES LIKE '%version%';"
 mysql --user=test --password=test -h 127.0.0.1 testDB <scripts/create-tables.sql
 mysql --user=test --password=test -h 127.0.0.1 testDB <scripts/load-data.sql
