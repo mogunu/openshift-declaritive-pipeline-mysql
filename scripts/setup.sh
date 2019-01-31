cc5d3ee26ce4e5a816a20d847e4e9cb21e272b3f
Ryan Spencer <ryan.spencer@gtri.gatech.edu>
2019-01-31 12:09:11 -0500

    fix mysql login

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -u myuser -pmypassword -h 127.0.0.1 testDB <scripts/create-tables.sql
-mysql -u myuser -pmypassword -h 127.0.0.1 testDB <scripts/load-data.sql
+mysql --user=test --password=test -h 127.0.0.1 testDB <scripts/create-tables.sql
+mysql --user=test --password=test -h 127.0.0.1 testDB <scripts/load-data.sql
