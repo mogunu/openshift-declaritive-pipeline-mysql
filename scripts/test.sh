cec91cd0c7f4a17d0e3e528f9b546b709c51829b
Ryan Spencer <ryan.spencer@gtri.gatech.edu>
2019-01-31 14:02:53 -0500

    print mysql version numbers

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,6 @@ pwd
 ls -alh
 
 # connect to mysql-server container, run a SQL QUERY
-mysql -v
+mysql -V
 mysql --user=test --password=test --host=127.0.0.1 --port=3306 --protocol=TCP -e "show databases;"
 mysql --user=test --password=test --host=127.0.0.1 --port=3306 --protocol=TCP testDB -e "SELECT * FROM users;"
